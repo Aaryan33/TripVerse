5aec6cc425f05dda50cb7cb1efde3b9232a5aea5
Aaryan Patel <97274994+Aaryan33@users.noreply.github.com>
2026-02-27 15:19:19 +0530

    Changes added
    Updated BASE_URL to point to the deployed backend.

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = process.env.REACT_APP_BASE_URL;
-export const BASE_URL = 'http://localhost:4000/api/v1';
+// export const BASE_URL = 'http://localhost:4000/api/v1';
+export const BASE_URL = 'https://tripverse-complete-backend.onrender.com/api/v1';   // Fetch data from deployed backend
